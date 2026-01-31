e5da5b5a4932c39881054f4dd48c79cb0bcad1cd
SameerUH <sameerulhaq2005@gmail.com>
2026-01-31 23:21:47 +0000

    Added scoring system to the game which was fun! Implemented via checking the filename of the masks and scoring based on it, Might need to make the game easier and slower at the start and progressively get quicker, need to test it more tomorrow though, also may need to fix the hitboxes tomorrow.

--- main.py
+++ main.py
@@ -25,16 +25,19 @@ covid_images = []
 face_images = []
 
 for i in range(1, 20):
+    path = f"Masquerade/mask_{i}.png"
     img = pygame.image.load(f"Masquerade/mask_{i}.png").convert_alpha()
-    mask_images.append(img)
+    mask_images.append((img, path))
 
 for i in range(1, 9):
+    path = f"COVID/covid_{i}.png"
     img = pygame.image.load(f"COVID/covid_{i}.png").convert_alpha()
-    covid_images.append(img)
+    covid_images.append((img, path))
 
 for i in range(1, 9):
+    path = f"Faces/face_{i}.png"
     img = pygame.image.load(f"Faces/face_{i}.png").convert_alpha()
-    face_images.append(img)
+    face_images.append((img, path))
 
 allmasks = [mask_images, covid_images, face_images]
 
@@ -45,25 +48,32 @@ class Mask:
         self.width = width
         self.height = height
         self.velocity = velocity
-        self.original_image = pygame.transform.scale(rand.choice(allmasks), (width, height))
+        image, self.filename = rand.choice(rand.choice(allmasks))
+        self.original_image = pygame.transform.scale(image, (self.width, self.height))
         self.image = self.original_image.copy()
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
         self.angle = 0
         self.rotation_speed = rand.randint(-3, 3)
-        self.hitbox = (self.rect.x, self.rect.y, 55, 55)
+        self.hitbox = (self.rect.x - 5, self.rect.y - 10, 110, 110)
 
     def draw_updatescreen(self):
         SCREEN.blit(self.image, self.rect)
-        self.hitbox = (self.rect.x + 5, self.rect.y + 5, 100, 100)
-        pygame.draw.rect(SCREEN, "red", self.hitbox, 2)
+        self.hitbox = (self.rect.x - 5, self.rect.y - 10, 110, 110)
+        #pygame.draw.rect(SCREEN, "red", self.hitbox, 2)
 
     def movement(self):
         if self.rect.y < 800:
             self.rect.y += self.velocity
+            return 0
         else:
+            old_filename = self.filename
             self.velocity = rand.randint(3, 10)
             self.rect.y = 0
-            self.original_image = pygame.transform.scale(rand.choice(rand.choice(allmasks)), (self.width, self.height))
+            image, self.filename = rand.choice(rand.choice(allmasks))
+            self.original_image = pygame.transform.scale(image, (self.width, self.height))
+            if "Masquerade" in old_filename:
+                return -20
+            return 0
     
     def rotate(self):
         self.angle = (self.angle + self.rotation_speed) % 360
@@ -76,22 +86,38 @@ class Mask:
             if mouse_pos[0] > self.hitbox[0] and mouse_pos[0] < self.hitbox[0] + self.hitbox[2]:
                 if mouse_clicked:
                     self.rect.y = 0
-                    self.original_image = pygame.transform.scale(rand.choice(rand.choice(allmasks)), (self.width, self.height))
+                    image, self.filename = rand.choice(rand.choice(allmasks))
+                    self.original_image = pygame.transform.scale(image, (self.width, self.height))
+                    if "COVID" in self.filename:
+                        return 5
+                    elif "Masquerade" in self.filename:
+                        return 20
+                    elif "face" in self.filename:
+                        return -10
+
+        return 0
 
     def update(self):
-        self.movement()
         self.rotate()
         self.draw_updatescreen()
-        self.interaction(mouse_pos, mouse_clicked)
+        score = self.interaction(mouse_pos, mouse_clicked)
+        score += self.movement()
+        return score
 
 
 objects = []
 
 x_coordinates = [i for i in range(100, 1800, 200)]
 for x in x_coordinates:
-    objects.append(Mask(x, 0, 100, 100, 2, allmasks[rand.randint(0, 2)]))
+    objects.append(Mask(x, 0, 100, 100, 2, allmasks))
 
 mouse_clicked = False
+
+player_score = 100
+player_score_font = pygame.font.Font('freesansbold.ttf', 50)
+player_score_text = player_score_font.render("SCORE: " + str(player_score), True, "black")
+player_score_rect = player_score_text.get_rect()
+player_score_rect.center = (150, 950)
 
 while True:
     mouse_pos = pygame.mouse.get_pos()
@@ -111,9 +137,11 @@ while True:
     SCREEN.fill(BACKGROUND_COLOUR)
 
     for object in objects:
-        object.update()
+        player_score += object.update()
 
     floor = pygame.draw.rect(SCREEN, "white", (0, 900, 1950, 10))
+    player_score_text = player_score_font.render("SCORE: " + str(player_score), True, "black")
+    SCREEN.blit(player_score_text, (player_score_rect))
 
     clock.tick(60)
 
